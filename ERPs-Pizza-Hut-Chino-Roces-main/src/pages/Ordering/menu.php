ace6d3b9e37d7614cdcd7fde0114c6ab03993e01
Kevin <mamadaddyalexa@gmail.com>
2024-12-01 20:56:28 +0800

    test

--- ERPs-Pizza-Hut-Chino-Roces-main/src/pages/Ordering/menu.php
+++ ERPs-Pizza-Hut-Chino-Roces-main/src/pages/Ordering/menu.php
@@ -12,7 +12,7 @@ if (isset($_SESSION['uid'])) {
     // Retrieve the current user's ID from the session
     $currentUserId = $_SESSION['uid'];
 
-    $sql = "SELECT address FROM customerInfo WHERE uid = $currentUserId"; // Replace 'users' with your table name
+    $sql = "SELECT address FROM customerinfo WHERE uid = $currentUserId"; // Replace 'users' with your table name
     $result = $conn->query($sql);
     $hasActiveOrders = false;
     $orderStatuses = ["placed", "preparing", "delivery"];

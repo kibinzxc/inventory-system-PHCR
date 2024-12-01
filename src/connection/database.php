e7661ce2aa30a29575895f0abdd08f3d7871edfc
Kevin <mamadaddyalexa@gmail.com>
2024-12-01 21:16:27 +0800

    test

--- src/connection/database.php
+++ src/connection/database.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$dbUsername = 'root';
-$dbPassword =  '';
-$dbName = 'phcr_db';
+$servername = "localhost";
+$username = "u560143421_kibinzxc";
+$password = "Kevin0405!";
+$database = "u560143421_phcr_db";
 
 $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
 

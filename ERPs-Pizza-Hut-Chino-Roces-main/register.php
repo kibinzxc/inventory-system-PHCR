2f15cf94fabdde94e2257eed54fc9575199c52ed
Kevin <mamadaddyalexa@gmail.com>
2024-12-01 21:41:54 +0800

    test

--- ERPs-Pizza-Hut-Chino-Roces-main/register.php
+++ ERPs-Pizza-Hut-Chino-Roces-main/register.php
@@ -59,7 +59,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
-    <link rel="icon" href="assets/img/pizzahut-logo.png">
+    <link rel="icon" href="src/assets/img/pizzahut-logo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="src/bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="src/bootstrap/css/bootstrap.min.css">

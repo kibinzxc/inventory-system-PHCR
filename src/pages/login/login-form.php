17891d6e96ac688f1009747205d756f239386c6c
Kevin <mamadaddyalexa@gmail.com>
2024-12-01 20:45:57 +0800

    revert changes

--- src/pages/login/login-form.php
+++ src/pages/login/login-form.php
@@ -12,7 +12,7 @@
 <body>
     <div class="LoginForm">
         <div class="LoginForm_header">
-            <h1>Login TEST12345</h1>
+            <h1>Login</h1>
         </div>
 
         <?php if (isset($_GET['error'])): ?>

f5bd27b072412a011178f7570de899e607124b1f
Kevin <mamadaddyalexa@gmail.com>
2024-12-09 05:57:23 +0800

    test

--- ERPs-Pizza-Hut-Chino-Roces-main/forgot-password.php
+++ ERPs-Pizza-Hut-Chino-Roces-main/forgot-password.php
@@ -6,9 +6,9 @@ include 'src/db/config.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '../../../phpmailer/src/Exception.php';
-require '../../../phpmailer/src/PHPMailer.php';
-require '../../../phpmailer/src/SMTP.php';
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Check connection
